2e2eca9a142d98212077a02b80ef57c7daccd22c
Vitalina-Oleksiienko <vitalina_oleksiienko@yahoo.com>
2021-11-23 12:57:35 +0200

    add mobile menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,6 +1,6 @@
 
 let menuBtn = document.querySelector('.menu-btn');
-let menu = document.querySelector('.header-menu');
+let menu = document.querySelector('.header-menu--js');
 menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
